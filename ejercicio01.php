daab71153624acc1e3d207a3921da43c319491b2
Adrian Calderon <drian95@gmail.com>
2016-09-30 18:36:23 +0200

    Ejercicio

--- ejercicio01.php
+++ ejercicio01.php
@@ -1,1 +1,21 @@
-TODO:
+<!DOCTYPE html> 
+<html><head> 
+        <meta charset="UTF-8"> 
+        <title>ejemplo 5</title> 
+</head><body>
+<?php
+echo "Inicializamos el array";
+$array1[0] = "Alicia";
+$array1[1] = "Andrea";
+$array1[2] = "Juana";
+$array1[3] = "Hermenegilda";
+$array1[4] = "Graciana";
+
+echo "<hr>Recorrido del bucle"; 
+echo "<hr>";
+foreach ($array1 as $element){ 
+    echo $element . '<br>'; 
+}  
+?>
+</body></html>
+
